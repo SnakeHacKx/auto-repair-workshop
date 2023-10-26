5b25636007e77d7acc65784484e38ec83a34e367
SnakeHacKx <omaralexisxdx2@gmail.com>
2023-10-26 17:51:21 -0500

    folders added

---
+++ src/php/borrar.php
@@ -0,0 +1,5 @@
+<?php
+file_put_contents("../../assets/autos.csv", "");
+header("Location: informe.php");
+exit();
+?>
---
+++ src/php/procesar.php
@@ -0,0 +1,20 @@
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $placa = $_POST["placa"];
+    $modelo = $_POST["modelo"];
+    $marca = $_POST["marca"];
+    $color = $_POST["color"];
+    $dueño = $_POST["dueño"];
+    $mantenimiento = $_POST["mantenimiento"];
+    $fecha = $_POST["fecha"];
+    $cubiculo = $_POST["cubiculo"];
+
+    // Guardar datos en archivo CSV
+    $csvData = "$placa,$modelo,$marca,$color,$dueño,$mantenimiento,$fecha,$cubiculo\n";
+    file_put_contents("../../assets/autos.csv", $csvData, FILE_APPEND);
+
+    // Redirigir de nuevo al formulario después de registrar
+    header("Location: index.html");
+    exit();
+}
+?>
